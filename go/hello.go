e5fba556de953858d29ccd88b53fd9de18792c6b
vpra711 <vpr.achutharaman1000@outlook.com>
2022-12-12 18:57:57 +0530

    go begin

--- go/hello.go
+++ go/hello.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"reflect"
+	"strconv"
+)
+
+var print = fmt.Println //alias
 
 func main() {
+
 	fmt.Println("Heelo World")
+
+	var a = 2
+	b := 3
+	var c int
+	c = 4
+
+	print(a, b, c)
+
+	//io_ops()
+	//datatype_cast()
 }
+
+func io_ops() {
+
+	print("Now in io_ops")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	name, err := reader.ReadString('\n')
+
+	if err == nil	{
+		print(name)
+	} else {
+		print("Some error occured")
+	}
+}
+
+func datatype_cast() {
+
+	print(reflect.TypeOf(25))
+	print(reflect.TypeOf(2.5))
+	print(reflect.TypeOf('2'))
+	print(reflect.TypeOf("25"))
+	print(reflect.TypeOf(true))
+
+	a := 1
+	b := float64(1)
+	c := "Heelo"
+	d, err := strconv.Atoi(c)
+
+	print(a, b, c, d, err)
+}
+
+
