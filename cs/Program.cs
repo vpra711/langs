7e811bb435aa19e77937b033236e1e383645145e
vpra <vpr.achutharaman1000@gmail.com>
2023-12-22 22:52:58 +0530

    csharp lang init

---
+++ cs/Program.cs
@@ -0,0 +1,19 @@
+﻿using cs.Sorting;
+
+namespace cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Sorting sorting = new Sorting();
+            int[] sortedArray = sorting.BubbleSort( new int[]{ 5, 4, 3, 2, 1 });
+
+            foreach( int i in sortedArray ) 
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ cs/Sorting/Sorting.cs
@@ -0,0 +1,33 @@
+﻿namespace cs
+{
+    internal class Sorting
+    {
+        public int[] BubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int temp = array[j];
+                        array[j] = array[i];
+                        array[i] = temp;
+                    }
+                }
+            }
+
+            return array;
+        }
+
+        public int[] SleepSort(int[] array)
+        {
+            return array;
+        }
+
+        public int[] MergeSort(int[] array)
+        {
+            return array;
+        }
+    }
+}
